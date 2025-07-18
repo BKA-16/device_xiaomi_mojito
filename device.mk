2bb12f6e17d6e934a3d00703246f72164461044b
Sachinpawar86 <sachinbarange86@gmail.com>
2025-07-18 19:30:50 +0530

    Revert "mojito: Add ViPER4AndroidFX Support"
    This reverts commit 0b6a10a2a2af46b2c0881eefcf3655d6b549f0e2.

--- device.mk
+++ device.mk
@@ -96,6 +96,3 @@ $(call inherit-product, vendor/xiaomi/mojito/mojito-vendor.mk)
 
 # Call the Leica Camera setup
 $(call inherit-product-if-exists, vendor/xiaomi/mojito-leicacamera/mojito-leicacamera-vendor.mk)
-
-# ViPER4AndroidFX
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
